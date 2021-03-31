5546a83f96dd90a879d0d5a005dcdaf36c91886a
Matthew2020-hub <akinolatolulope24@gmail.com>
2021-03-31 13:18:49 +0100

    first push to github

---
+++ zuri.py
@@ -0,0 +1,44 @@
+import datetime
+now= datetime.datetime.now()
+print(now.strftime("%Y-%d-%m"))
+print(now.strftime("%H %M: %S"))
+
+name = input ("What's your name\n")
+allowedUsers = ['Seyi', 'Makinde', 'Matthew']
+allowedPassword = ['password','password', 'password']
+
+if (name in allowedUsers):
+    Password= input ("What's your password \n")
+    userId = allowedUsers.index(name)
+
+try:
+
+if (Password == allowedPassword[userId]):
+
+    print(f'Welcome home {name}')
+    print('These are the available options: ')
+    print('1. Withdrawal')
+    print('2. Cash Deposit')
+    print('3. Complaint')
+
+selectedoption = int(input('Please select an option: '))
+
+
+if(selectedoption == 1):
+    withdrawal= int(input ('How much would you like to withdraw \n'))
+    print('Take your cash')
+
+elif(selectedoption == 2):
+     deposit= int(input ('How much would you like to deposit \n'))
+    print(f'Current balance is {deposit}')
+
+else:
+    complaint= (input ('What issue will you like to report \n'))
+
+    print('Thank you for contactacting us')
+else:
+    print("Password incorrect, try again")
+else:
+    print("Invalid name")
+except:
+        print("please enter digits not alphabets")
